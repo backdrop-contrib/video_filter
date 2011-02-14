a4216a2e5400e512121d39a9a08b8e786a7f775a
Emil Stjerneman <emil@stjerneman.com>
2011-02-14 13:58:29 +0000

    by anon: Added global JS settings (video_filter.modulepath)

--- editors/fckeditor/video_filter/fckplugin.js
+++ editors/fckeditor/video_filter/fckplugin.js
@@ -1,15 +1,11 @@
 // $Id$
 
-if(typeof Drupal != 'undefined') {
-  var path = Drupal.settings.video_filter.url.wysiwyg_fckeditor;
-  var basePath =  Drupal.settings.basePath;
-} else {
-  var basePath =  video_filter_basePath;
-  var path = basePath + video_filter_url_fckeditor;
-}
+var path = Drupal.settings.video_filter.url.wysiwyg_fckeditor;
+var basePath =  Drupal.settings.basePath;
+var modulePath = Drupal.settings.video_filter.modulepath;
 
 FCKCommands.RegisterCommand( 'video_filter', new FCKDialogCommand( 'video_filter', '&nbsp;', path, 580, 480 ) ) ;
 
 var oVideoFilterItem = new FCKToolbarButton( 'video_filter', 'video_filter');
-oVideoFilterItem.IconPath = basePath + 'sites/all/modules/video_filter/editors/fckeditor/video_filter/video_filter.png';
+oVideoFilterItem.IconPath = basePath + modulePath + '/editors/fckeditor/video_filter/video_filter.png';
 FCKToolbarItems.RegisterItem( 'video_filter', oVideoFilterItem );
